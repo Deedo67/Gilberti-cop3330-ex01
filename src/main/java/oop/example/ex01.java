4ae298acf5276abd89c582489284ef5f1acb4b92
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 16:41:28 -0400

    Put in Gitignore, Main code finish.

--- src/main/java/oop/example/ex01.java
+++ src/main/java/oop/example/ex01.java
@@ -1,4 +1,6 @@
 package oop.example;
+
+import java.util.Scanner;
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Derek Gilberti
@@ -6,6 +8,17 @@ package oop.example;
 public class ex01 {
     // main method begin
     public static void main(String[] args) {
-        System.out.println("What is your name?");
+        //Scanner
+        Scanner in = new Scanner(System.in);
+
+        //Input name
+        System.out.print("What is your name?");
+        String name = in.nextLine();
+
+        //Output String
+        String outputString = "Hello, " + name + ", nice to meet you!";
+
+        //Output
+        System.out.println(outputString);
     } // end main method
 } // end class

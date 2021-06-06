12824d94ae6703e2b74683a52a655b980d48f263
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 16:16:50 -0400

    Initial commit

---
+++ src/main/java/oop/example/ex01.java
@@ -0,0 +1,11 @@
+package oop.example;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+public class ex01 {
+    // main method begin
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+    } // end main method
+} // end class
